a805bcc8cb12873316dada53def3723f8a045bfb
Joe Vrolijk <joe.vrolijk@gmail.com>
2020-05-23 21:46:02 +0200

    Need to fix some stuff

--- App/app/src/main/java/com/team1/covid19info/ui/feed/FeedFragment.kt
+++ App/app/src/main/java/com/team1/covid19info/ui/feed/FeedFragment.kt
@@ -1,12 +1,14 @@
 package com.team1.covid19info.ui.feed
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -37,7 +39,7 @@ class FeedFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         initViews()
         initViewModel()
-//        viewModel = FeedViewModel(requireContext())
+
     }
 
     private fun initViews(){
@@ -46,12 +48,19 @@ class FeedFragment : Fragment() {
     }
 
     private fun initViewModel(){
-        viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
-        viewModel.newsItems.observe(this, Observer {
-            this.newsItems
+//        viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
+        viewModel = FeedViewModel(requireContext())
+        viewModel.newsItems.observe(viewLifecycleOwner, Observer {
+            viewModel.getCovidNews()
         })
+    }
+
+    private fun onNewsItemClick(newsItem: NewsItem){
+        Toast.makeText(context, "This is a test! Popup Screen Now! - " + newsItem.title, Toast.LENGTH_LONG);
 
     }
+
+
 
 
 
