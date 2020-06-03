a5dda3c9ef00af8cc7fd7e0461716c71df1f2f3f
evaanp <evanplat1@hotmail.com>
2020-06-03 16:47:53 +0200

    COV-59: kleuren consistent gemaakt

--- App/app/src/main/java/com/team1/covid19info/ui/MainActivity.kt
+++ App/app/src/main/java/com/team1/covid19info/ui/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tbToolbar.setNavigationIcon(R.drawable.ic_covid19_black_100dp)
         initNavigation()
     }
 
