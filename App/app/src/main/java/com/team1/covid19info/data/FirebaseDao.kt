22ce30a7d6fb583708d46f0d5f44d2a6ce3cbaf1
Joe Vrolijk <joe.vrolijk@gmail.com>
2020-06-06 20:35:39 +0200

    Only Read is left

--- App/app/src/main/java/com/team1/covid19info/data/FirebaseDao.kt
+++ App/app/src/main/java/com/team1/covid19info/data/FirebaseDao.kt
@@ -1,21 +1,29 @@
 package com.team1.covid19info.data
 
 
+import android.content.ContentValues.TAG
 import android.util.Log
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.getValue
 import com.team1.covid19info.model.NewsItem
+import java.sql.Time
+import java.sql.Timestamp
+import java.time.Instant
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
 
 class FirebaseDao {
+
+
     val database = FirebaseDatabase.getInstance()
     val newsItemsCollection = database.getReference("newsItems")
     val lastUpdatedCollection = database.getReference("lastUpdated")
+
+
+    private lateinit var timestampReference: DatabaseReference
+    private var timestampListener: ValueEventListener? = null
+    private var lastUpdated: Timestamp? = null
 
 
     fun refreshNewsItems(newsItems: List<NewsItem>) {
@@ -26,30 +34,22 @@ class FirebaseDao {
     }
 
 
-    fun addFirstInstant() {
-        val firstDateTime = LocalDateTime.now().minus(1, ChronoUnit.HOURS)
+    fun addFirstTimestamp() {
+        val firstDateTime = Timestamp.from(Instant.now().minus(1, ChronoUnit.HOURS))
         lastUpdatedCollection.setValue(firstDateTime)
     }
 
-    fun getLastUpdateTime(): LocalDateTime {
-        var instantReturned = LocalDateTime.now()
-        lastUpdatedCollection.addValueEventListener(object : ValueEventListener {
 
-            override fun onCancelled(p0: DatabaseError) {
-                Log.w("LastUpdate could not be read from the DB", p0.toString())
-            }
-
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val lastUpdate = dataSnapshot.getValue<LocalDateTime>()
-                if (lastUpdate == null) {
-                    addFirstInstant()
-                } else {
-                    instantReturned = lastUpdate!!
-                    Log.d("******", "Value = $lastUpdate")
+    fun getLastUpdateTime(){
+        lastUpdatedCollection.child("lastUpdated")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
                 }
-            }
-        })
-        return instantReturned
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    lastUpdated = p0.value as Timestamp
+                }
+            })
     }
 
     fun getNewsItems(): List<NewsItem> {
@@ -67,3 +67,12 @@ class FirebaseDao {
         return newsItems
     }
 }
+
+
+
+
+//get last datetime from fb
+// if last datetime < now - hour then call api
+// then: clear old newsitems, store newsItems to fb, and add to mutablelist and insert new datetime
+// else: clear old newsitems, retrieve newsItems in fb and add to mutableList
+
--- App/app/src/main/java/com/team1/covid19info/ui/feed/FeedViewModel.kt
+++ App/app/src/main/java/com/team1/covid19info/ui/feed/FeedViewModel.kt
@@ -1,11 +1,18 @@
 package com.team1.covid19info.ui.feed
 
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.getValue
 import com.team1.covid19info.data.FirebaseDao
 import com.team1.covid19info.data.NewsFeedRepository
 import com.team1.covid19info.model.NewsItem
 import com.team1.covid19info.ui.ViewModelBase
+import kotlinx.coroutines.withContext
+import java.sql.Timestamp
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.*
@@ -13,24 +20,93 @@ import java.util.*
 class FeedViewModel(context: Context) : ViewModelBase(context) {
 
     private val newsRepository = NewsFeedRepository()
-    private val firebaseDao = FirebaseDao()
+    val database = FirebaseDatabase.getInstance()
+    val newsItemsCollection = database.getReference("newsItems")
+    val lastUpdatedCollection = database.getReference("lastUpdated")
+
+    private lateinit var instantReference: DatabaseReference
+    private var instantListener: ValueEventListener? = null
+    private var lastUpdated: Instant? = null
 
     val newsItems = MutableLiveData<List<NewsItem>>()
 
     fun getCovidNews(){
-        val lastUpdated = firebaseDao.getLastUpdateTime()
-        val localDateTime = Date.from(Instant.now().minus(1, ChronoUnit.HOURS))
-        if (lastUpdated.isBefore(localDateTime.toInstant())) {
-            calls.addLast {
-                val response = newsRepository.getCovidNews()
-                firebaseDao.refreshNewsItems(response.newsItems)
-                newsItems.postValue(response.newsItems)
-            }
+        getLastUpdateTime()
+        if (lastUpdated!!.isBefore(Instant.now().minus(1, ChronoUnit.HOURS))){
+            refreshNewsItems()
         } else {
-            val retrievedNewsItems = firebaseDao.getNewsItems()
-            newsItems.postValue(retrievedNewsItems)
+            getDbNewsItems()
         }
     }
+
+
+    fun insertInstant() {
+        lastUpdatedCollection.removeValue()
+        val firstDateTime = Instant.now()
+        lastUpdatedCollection.setValue(firstDateTime)
+    }
+
+
+    fun getLastUpdateTime(){
+
+        val ref = lastUpdatedCollection
+        val instantRef = ref.child("lastUpdated")
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val instantVal = dataSnapshot.child("lastUpdated").getValue(Instant::class.java)
+                val instantChild = dataSnapshot.child("nano").getValue(Long::class.java)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d(TAG, databaseError.getMessage()) //Don't ignore errors!
+            }
+        }
+
+        instantRef.addListenerForSingleValueEvent(valueEventListener);
+
+
+
+//
+//        val instantListenerObject = object : ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                val instant = dataSnapshot.getValue<Instant>()
+//                lastUpdated = instant
+//            }
+//
+//            override fun onCancelled(databaseError: DatabaseError) {
+//                // Getting Post failed, log a message
+//                Log.w(TAG, "JoeRene", databaseError.toException())
+//            }
+//        }
+//        instantReference.addValueEventListener(instantListenerObject)
+//        this.instantListener = instantListenerObject
+    }
+
+    fun getDbNewsItems(){
+        newsItemsCollection.child("newsItems")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    newsItems.postValue(p0.value as List<NewsItem>)
+                }
+            })
+    }
+
+
+    fun refreshNewsItems() {
+        newsItemsCollection.removeValue()
+        calls.addLast {
+            val response = newsRepository.getCovidNews()
+            newsItemsCollection.setValue(response).addOnSuccessListener {
+                Log.i("Update Success", "Updating NewsItems was successful")
+                insertInstant()
+            }
+            newsItems.postValue(response.newsItems)
+        }
+    }
+
 }
 
 
@@ -38,3 +114,10 @@ class FeedViewModel(context: Context) : ViewModelBase(context) {
 //    val response = newsRepository.getCovidNews()
 //    newsItems.postValue(response.newsItems)
 //}
+
+
+//get last datetime from fb
+// if last datetime < now - hour then call api
+// then: clear old newsitems, store newsItems to fb, and add to mutablelist and insert new datetime
+// else: clear old newsitems, retrieve newsItems in fb and add to mutableList
+
