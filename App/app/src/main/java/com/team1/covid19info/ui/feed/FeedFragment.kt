825c7f936e43a86f409dbc8b7e80a77501df0822
Joe Vrolijk <joe.vrolijk@gmail.com>
2020-06-07 18:44:37 +0200

    Firebase implementatie is done and tested

--- App/app/src/main/java/com/team1/covid19info/ui/feed/FeedFragment.kt
+++ App/app/src/main/java/com/team1/covid19info/ui/feed/FeedFragment.kt
@@ -74,7 +74,7 @@ class FeedFragment : Fragment() {
                 viewModel.getCovidNews()
             }
         }
-        timer.scheduleAtFixedRate(task, 10000, 1000)
+        timer.scheduleAtFixedRate(task, 100000, 1000)
     }
 
 
